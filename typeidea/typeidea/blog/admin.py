b44221edc754ddd0bd72498c9b90d928d61cdc59
进击的团子 <jpch89@outlook.com>
2019-07-09 09:34:06 +0800

    add 6.3

--- typeidea/typeidea/blog/admin.py
+++ typeidea/typeidea/blog/admin.py
@@ -24,3 +24,90 @@ class CategoryAdmin(BaseOwnerAdmin):
         return obj.post_set.count()
 
     post_count.short_description = '文章数量'
+
+
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """自定义过滤去只展示当前用户分类"""
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(
+            owner=request.user).values_list(
+            'id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
+@admin.register(Post, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'owner', 'operator'
+    ]
+    list_display_links = []
+
+    list_filter = [CategoryOwnerFilter, ]
+    search_fields = ['title', 'category__name']
+    save_on_top = True
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    exclude = ['owner']
+    """
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+    """
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('wide', ),
+            'fields': ('tag', ),
+        }),
+    )
+    # filter_horizontal = ('tag', )
+    filter_vertical = ('tag', )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    class Media:
+        css = {
+            'all': (
+                "https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css",
+            ),
+        }
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js', )
--- typeidea/typeidea/config/admin.py
+++ typeidea/typeidea/config/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 from .models import Link, SideBar
 from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
 
 
 @admin.register(Link, site=custom_site)
-class LinkAdmin(admin.ModelAdmin):
+class LinkAdmin(BaseOwnerAdmin):
     list_display = (
         'title',
         'href',
@@ -21,7 +22,7 @@ class LinkAdmin(admin.ModelAdmin):
 
 
 @admin.register(SideBar, site=custom_site)
-class SideBarAdmin(admin.ModelAdmin):
+class SideBarAdmin(BaseOwnerAdmin):
     list_display = ('title', 'display_type', 'content', 'created_time')
     fields = ('title', 'display_type', 'content')
 
