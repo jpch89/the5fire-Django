a0168d5cc7291b00d4b2d5ae7a74b46d27351194
进击的团子 <jpch89@outlook.com>
2019-07-09 10:11:33 +0800

    fix bug 6.3

--- typeidea/typeidea/comment/admin.py
+++ typeidea/typeidea/comment/admin.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 
 from .models import Comment
 from typeidea.custom_site import custom_site
-from typeidea.base_admin import BaseOwnerAdmin
 
 
 @admin.register(Comment, site=custom_site)
-class CommentAdmin(BaseOwnerAdmin):
+class CommentAdmin(admin.ModelAdmin):
     list_display = (
         'target',
         'nickname',
