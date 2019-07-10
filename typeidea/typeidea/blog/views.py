881d55b862544eb63f7a66da488f6eb1511caa14
进击的团子 <jpch89@outlook.com>
2019-07-10 15:31:38 +0800

    add 7.2

--- typeidea/typeidea/blog/views.py
+++ typeidea/typeidea/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Post, Tag, Category
+from config import SideBar
 
 
 def post_list(request, category_id=None, tag_id=None):
@@ -18,7 +19,22 @@ def post_list(request, category_id=None, tag_id=None):
         'category': category,
         'tag': tag,
         'post_list': post_list,
+        'sidebars': SideBar.get_all(),
     }
     context.update(Category.get_navs())
 
     return render(request, 'blog/list.html', context=context)
+
+
+def post_detail(request, post_id=None):
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+
+    context = {
+        'post': post,
+        'sidebars': SideBar.get_all()
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/detail.html', context=context)
